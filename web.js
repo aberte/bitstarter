98dea2ee59766650ccd890724eb1e0d01d9eeb02
Andrés Berte <aberte@gmail.com>
2013-07-15 23:06:10 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var filename = 'index.html';
+  var buffer = fs.readFileSync(filename);
+  response.send(buffer.toString());
+//   response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
